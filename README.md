7f55deadbfdb3ceb2212a19268b4d3866ab3340a
Javier Sánchez Rois <jsanchez@gradiant.org>
2015-02-17 10:59:21 +0100

    HULKENIZERgit status  initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+[![Build Status](https://travis-ci.org/jsrois/hkn_refactoring.svg?branch=master)](https://travis-ci.org/jsrois/hkn_refactoring)
+
+# hkn_refactoring
+
+
+## Install
+
+1. Set environment variable OPENCV_PATH to your OpenCV location (e.g. /usr/local)
+2. run `qmake` to generate makefile 
+3. run `make`
---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main()
+{
+
+    VideoCapture c1("data/trellis.avi");
+//    VideoCapture video("test.avi");
+
+    CascadeClassifier
+            clasif("data/haarcascade_frontalface_alt2.xml");
+
+    while (1)
+    {
+        Mat myImage,greyimage,rgb;
+
+        c1 >> myImage;
+//        c1.read(rgb);
+        if (myImage.empty()) break;
+        vector<Rect> detecciones_vector;
+        clasif.detectMultiScale(myImage,detecciones_vector,1.1, 3, CV_HAAR_FIND_BIGGEST_OBJECT, Size(30, 30), Size(200,200));
+        vector<Mat> canalesIm;
+        split(myImage,canalesIm);
+
+//        for (int i=0;i<detecciones_vector.size();i++)
+//        {
+//            vector<Mat> canalesIm;
+//            split(myImage,canalesIm);
+//            canalesIm[1](detecciones_vector[i]) = 2*canalesIm[1](detecciones_vector[i]);
+//        }
+
+        for (int i=0;i<detecciones_vector.size();i++)
+        {
+            Mat face = canalesIm[1](detecciones_vector[i]);
+            face = 2*face;
+        }
+
+        merge(canalesIm,myImage);
+
+        for (int i=0;i<detecciones_vector.size();i++)
+            rectangle(myImage,detecciones_vector[i],CV_RGB(255,0,0));
+
+        imshow("frame",myImage);
+        char k = waitKey(10);
+        if (k=='q') break;
+    }
+    return 0;
+}
